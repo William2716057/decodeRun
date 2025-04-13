57189684b40d3b74562cef6314df14332448f600
User <Willspri@gmail.com>
2025-04-13 21:03:24 +1200

    create new file on run

--- decodeRun.ps1
+++ decodeRun.ps1
@@ -5,4 +5,6 @@ $bytes = [System.Convert]::FromBase64String($base64Content)
 
 $decodedText = [System.Text.Encoding]::UTF8.GetString($bytes)
 
-Write-Host $decodedText
+#Write-Host $decodedText
+
+Set-Content -Path "decoded.ps1" -Value $decodedText
